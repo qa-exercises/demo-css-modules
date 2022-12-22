b287e8c6c12b6f9986f46c2011508e03c7125444
qa-exercises <118732445+qa-exercises@users.noreply.github.com>
2022-12-22 01:36:06 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# demo-css-modules
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/demo-css-modules)
---
+++ src/components/PageTitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PageTitle() {
+  return (
+    <header>
+      <h1 className="pageTitle">Page title</h1>
+    </header>
+  )
+}
